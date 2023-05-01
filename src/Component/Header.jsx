a928323269bf77fac61dd07551639085855204a8
rajveersinhvala <rajveersinhvala12@gmail.com>
2023-05-01 12:05:18 +0300

    changes

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -12,13 +12,13 @@ const Header = () => {
         h={"20"}
       >
         <stack style={{ postion: "left" }}>
-          <Button variant={"unstyled"} color={"white"}>
+          <Button variant={"unstyled"} color={"white"} ml={5}>
             <Link to={"/"}>Home</Link>
           </Button>
-          <Button variant={"unstyled"} color={"white"}>
+          <Button variant={"unstyled"} color={"white"} ml={5}>
             <Link to={"/exchanges"}>Exchange</Link>
           </Button>
-          <Button variant={"unstyled"} color={"white"}>
+          <Button variant={"unstyled"} color={"white"} ml={5}>
             <Link to={"/coins"}>Coins</Link>
           </Button>
         </stack>
